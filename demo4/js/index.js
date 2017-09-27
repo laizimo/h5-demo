adbd33e93e7c67771a3a4173df14f5073c3b93d7
来铧波 <1902056720@qq.com>
2017-09-27 15:50:50 +0800

    demo5

--- demo4/js/index.js
+++ demo4/js/index.js
@@ -11,7 +11,7 @@ const table = (function($, win){
       _self.setData();
     },
     fetch: function(){
-      _self = this;
+      const _self = this;
       console.log(11);
       return new Promise((resolve, reject) => {
         const url = _self.config.url + '/api/person';
@@ -21,7 +21,7 @@ const table = (function($, win){
           dataType: 'json',
           data: {},
           success: function(response){
-            res = JSON.parse(response);
+            const res = JSON.parse(response);
             if(res["code"] == 0){
               resolve(res.data);
             }else{
